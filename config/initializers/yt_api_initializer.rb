297056a7dc8318be06c0dba222690ea1136bfe0a
Caillou <igorlacroute@gmail.com>
2017-08-28 17:48:29 +0000

    add oauth keys to the initializer. Now need to get devise with oauth

--- config/initializers/yt_api_initializer.rb
+++ config/initializers/yt_api_initializer.rb
@@ -1,5 +1,7 @@
 class YtApiInitializer
     API_KEY = ENV["YOUTUBE_API_KEY"]
+    OAUTH_CLIENT_ID = ENV["YOUTUBE_OAUTH_CLIENT_ID"]
+    OAUTH_CLIENT_SECRET = ENV["YOUTUBE_OAUTH_CLIENT_SECRET"]
     
     if(API_KEY)
         Yt.configuration.api_key = API_KEY 
@@ -7,4 +9,15 @@ class YtApiInitializer
         raise LoadError, "Can't find environment variable 'YOUTUBE_API_KEY'"
     end
     
+    if(OAUTH_CLIENT_ID)
+        Yt.configuration.client_id = OAUTH_CLIENT_ID
+        
+        if(OAUTH_CLIENT_SECRET)
+            Yt.configuration.client_secret = OAUTH_CLIENT_SECRET
+        else 
+            raise LoadError, "Can't find environment variable 'YOUTUBE_OAUTH_CLIENT_SECRET'"
+        end
+    else
+        raise LoadError, "Can't find environment variable 'YOUTUBE_OAUTH_CLIENT_ID'"
+    end
 end
